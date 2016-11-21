08911961a18f889951789707feddcc1509e3ed04
slipmatt <Imtiaz.pocketpc@gmail.com>
2016-11-21 14:01:23 +0200

    Init Commit

--- LeagueManager/Class1.cs
+++ LeagueManager/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LeagueManager
 {
-    public class Class1
+    public class Class2
     {
     }
 }
---
+++ Leaguemanager.Tests/EventsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitOfWork;
+
+namespace Leaguemanager.Tests
+{
+    [TestFixture]
+    public class EventsTests
+    {
+        private IUow _uow { get; set; }
+        #region Setup / Teardown
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            
+        }
+
+        [TestFixtureTearDown]
+        public void Close()
+        {
+            
+        }
+        #endregion
+
+
+    }
+}
